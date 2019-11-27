42c35c40c6820fac099d6017eac8a27cd8cdc2d5
B <basiasiczek@gmail.com>
2019-11-27 23:50:24 +0100

    get stIndextLevel

--- main.js
+++ main.js
@@ -2,12 +2,24 @@ const fetch = require('node-fetch');
 const mainURL = 'http://api.gios.gov.pl/pjp-api/rest/';
 const getIndexURL = 'aqindex/getIndex/';
 
+//TODO: finish object creation
+function station(name){
+  this.name = name;
+  this.stationId = 0;
+  this.index = '';
+  this.param = [];
+}
+
 
 async function fetchIndex(url) {
     const response = await fetch(url);
     const json = await response.json();
-   
+    station.index = json.stIndexLevel.indexLevelName;
+    
+    //TODO: remove console.log
     console.log(json);
+    console.log(station.index);
+   
   }
    
-  fetchIndex('http://api.gios.gov.pl/pjp-api/rest/aqindex/getIndex/129');
+fetchIndex('http://api.gios.gov.pl/pjp-api/rest/aqindex/getIndex/129');
